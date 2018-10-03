f7be823b602c811a6828c50e5e894ce018902275
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2018-10-03 11:46:08 -0400

    Updated from Brython Server: 10/3/2018 11:46:08 AM

--- cryptography.py
+++ cryptography.py
@@ -1,6 +1,6 @@
 """
 cryptography.py
-Author: <your name here>
+Author: Tristan Meyer
 Credit: <list sources used, if any>
 
 Assignment:
@@ -10,3 +10,25 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations.find(char)
+print(associations)
+
+options = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+if options == "e":
+    message = input("Message: )
+    key = input("Key: )
+    
+#-------------------------------------------------------------------------------
+if options == "d":
+    emessage = input("Message: )
+    dkey = input("Key: )
+    
+#-------------------------------------------------------------------------------
+if options == "q":
+    
+if options != "e" and options != "d" and options != "q":
+    print("Did not understand command, try again."
+
+
+
