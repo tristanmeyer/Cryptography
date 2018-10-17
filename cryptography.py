02c108a8dd485113764e7a77603ec81e516b28f6
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2018-10-17 11:59:40 -0400

    Updated from Brython Server: 10/17/2018 11:59:40 AM

--- cryptography.py
+++ cryptography.py
@@ -11,59 +11,60 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-
-
-options = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-t = []
-m = []
-s = ""
-if options == "e":
-    message = list(input("Message: "))
-    lmessage = len(message)
-    key = list(input("Key: "))
-    nnkey = key * lmessage
-    for a in message:
-        nmessage = associations.find(a)
-        t.append(nmessage)
-    for b in nnkey:
-        nkey = associations.find(b)
-        m.append(nkey)
-    c = [(t + m) for t, m in zip(t, m)]
-    for d in c:
-        e = ''.join([associations[f % len(associations)] for f in c])
-    print(e)
-
-    
-#-------------------------------------------------------------------------------
-if options == "d":
-    message = list(input("Message: "))
-    lmessage = len(message)
-    key = list(input("Key: "))
-    nnkey = key * lmessage
-    for a in message:
-        nmessage = associations.find(a)
-        t.append(nmessage)
-    for b in nnkey:
-        nkey = associations.find(b)
-        m.append(nkey)
-    c = [(t - m) for t, m in zip(t, m)]
-    for d in c:
-        e = ''.join([associations[f % len(associations)] for f in c])
-    print(e)
-
-if options == "d" or options == "e": 
+def loop():
     options = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-    
-#-------------------------------------------------------------------------------
-if options == "q":
-    print("Goodbye!")
-    
-if options != "e" and options != "d" and options != "q":
-    print("Did not understand command, try again.")
-    options = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-
-
-
+    for g in options:
+        
+        t = []
+        m = []
+        s = ""
+        if options == "e":
+            message = list(input("Message: "))
+            lmessage = len(message)
+            key = list(input("Key: "))
+            nnkey = key * lmessage
+            for a in message:
+                nmessage = associations.find(a)
+                t.append(nmessage)
+            for b in nnkey:
+                nkey = associations.find(b)
+                m.append(nkey)
+            c = [(t + m) for t, m in zip(t, m)]
+            for d in c:
+                e = ''.join([associations[f % len(associations)] for f in c])
+            print(e)
+            loop()
+        
+            
+        #-------------------------------------------------------------------------------
+        elif options == "d":
+            message = list(input("Message: "))
+            lmessage = len(message)
+            key = list(input("Key: "))
+            nnkey = key * lmessage
+            for a in message:
+                nmessage = associations.find(a)
+                t.append(nmessage)
+            for b in nnkey:
+                nkey = associations.find(b)
+                m.append(nkey)
+            c = [(t - m) for t, m in zip(t, m)]
+            for d in c:
+                e = ''.join([associations[f % len(associations)] for f in c])
+            print(e)
+            loop()
+            
+        #-------------------------------------------------------------------------------
+        elif options == "q":
+            print("Goodbye!")
+            
+        #-------------------------------------------------------------------------------   
+        else:
+            print("Did not understand command, try again.")
+            loop()
+        
+           
+        
+loop()
+        
+        
